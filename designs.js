dad73a1f5a329d61b2abc96f89d7a27ef8c52d84
Kleber Santana <kleberdssantana@gmail.com>
2018-03-13 00:29:11 -0300

    first commit

---
+++ designs.js
@@ -0,0 +1,70 @@
+// Select color input
+// Select size input
+
+// const canvas = $('#pixelCanvas');
+const canvas = document.getElementById('pixelCanvas');
+const contextCanvas = canvas.getContext('2d');
+
+
+contextCanvas.moveTo(0, 20);
+contextCanvas.lineTo(300, 20);
+contextCanvas.moveTo(0, 40);
+contextCanvas.lineTo(300, 40);
+contextCanvas.moveTo(0, 60);
+contextCanvas.lineTo(300, 60);
+contextCanvas.moveTo(0, 80);
+contextCanvas.lineTo(300, 80);
+contextCanvas.moveTo(0, 100);
+contextCanvas.lineTo(300, 100);
+contextCanvas.moveTo(0, 120);
+contextCanvas.lineTo(300, 120);
+contextCanvas.moveTo(0, 140);
+contextCanvas.lineTo(300, 140);
+
+contextCanvas.moveTo(20, 0);
+contextCanvas.lineTo(20, 300);
+contextCanvas.moveTo(40, 0);
+contextCanvas.lineTo(40, 300);
+contextCanvas.moveTo(60, 0);
+contextCanvas.lineTo(60, 300);
+contextCanvas.moveTo(80, 0);
+contextCanvas.lineTo(80, 300);
+contextCanvas.moveTo(100, 0);
+contextCanvas.lineTo(100, 300);
+contextCanvas.moveTo(120, 0);
+contextCanvas.lineTo(120, 300);
+contextCanvas.moveTo(140, 0);
+contextCanvas.lineTo(140, 300);
+contextCanvas.moveTo(160, 0);
+contextCanvas.lineTo(160, 300);
+contextCanvas.moveTo(180, 0);
+contextCanvas.lineTo(180, 300);
+contextCanvas.moveTo(200, 0);
+contextCanvas.lineTo(200, 300);
+contextCanvas.moveTo(220, 0);
+contextCanvas.lineTo(220, 300);
+contextCanvas.moveTo(240, 0);
+contextCanvas.lineTo(240, 300);
+contextCanvas.moveTo(260, 0);
+contextCanvas.lineTo(260, 300);
+contextCanvas.moveTo(280, 0);
+contextCanvas.lineTo(280, 300);
+contextCanvas.moveTo(300, 0);
+contextCanvas.lineTo(300, 300);
+contextCanvas.stroke();
+//
+// contextCanvas.fillStyle = "rgba(0, 0, 200, 0.5)";
+// contextCanvas.fillRect (150, 0, 100,100);
+//
+// contextCanvas.fillRect(25, 25, 100, 100);
+// contextCanvas.clearRect(45, 45, 60, 60);
+// contextCanvas.strokeRect(50, 50, 50, 50);
+
+// console.log(contextCanvas);
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+
+// Your code goes here!
+
+}
